a7e68ba16400ff1e176ce1f090247372fd19fb53
LanzeG <bermas.lester10@gmail.com>
2024-06-24 16:16:12 +0800

    Add files via upload

---
+++ FIELD MONITORING JUNE/Frontend/charts.js
@@ -0,0 +1,4 @@
+document.getElementById('filter-icon').addEventListener('click', function() {
+  var filterContainer = document.getElementById('filters');
+  filterContainer.classList.toggle('hidden');
+});
